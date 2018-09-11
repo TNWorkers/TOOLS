c9cd074bb9aa19d73986aabbcbaa1284859c1ff3
Matthias <mpeschke@physnet.uni-hamburg.de>
2018-09-11 17:12:13 +0200

    zum Commit vorgemerkte Änderungen: 	geändert:       LiebWu.h
    	-Grundzustandsenergie für Hubbardmodell nach Bethe-Ansatz nun auch für beliebiges t.
    	 Dafür geändert: U -> U/t und gse -> t*gse.

--- LiebWu.h
+++ LiebWu.h
@@ -6,18 +6,20 @@
 
 struct LiebWu
 {
-	static double U;
-	
+	static double t;
+	static double U;	
 	static double e0_integrand (double x, void*)
 	{
-		return -4.*gsl_sf_bessel_J0(x)*gsl_sf_bessel_J1(x)/x/(1.+exp(x*U*0.5));
+		return -4.*t*gsl_sf_bessel_J0(x)*gsl_sf_bessel_J1(x)/x/(1.+exp(x*(U/t)*0.5));
 	}
 };
 double LiebWu::U=0.;
+double LiebWu::t=1.;
 
-double LiebWu_e0 (double U_input, double err=1e-9)
+double LiebWu_e0 (double U_input, double t_input = 1., double err=1e-9)
 {
 	LiebWu::U = U_input;
+	LiebWu::t = t_input;
 	return seminfigrate(LiebWu::e0_integrand, 0., err,err);
 }
 
