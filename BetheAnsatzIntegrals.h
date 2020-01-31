9ad8a7aaff6ee83778e23e0f982e95780484b926
rrausch <rrausch@physnet.uni-hamburg.de>
2020-01-31 19:08:35 +0900

    Changes to be committed: 	modified:   BetheAnsatzIntegrals.h
    - Namespace hinzugefuegt

--- BetheAnsatzIntegrals.h
+++ BetheAnsatzIntegrals.h
@@ -4,7 +4,9 @@
 #include "gsl_integration.h"
 #include <gsl/gsl_sf_bessel.h>
 
-struct BetheAnsatzIntegrals
+namespace BetheAnsatz{
+
+struct Integrals
 {
 	static double u;
 	static double e0_integrand (double x, void*)
@@ -17,18 +19,27 @@ struct BetheAnsatzIntegrals
 		return (x==0.)? 0.:2.*gsl_sf_bessel_J1(x)*exp(-x*u)/(x*cosh(x*u));
 	}
 };
-double BetheAnsatzIntegrals::u=0.;
+double Integrals::u=0.;
 
-double BetheAnsatz_e0 (double U_input, double err=1e-9)
+double e0 (double U_input, double err=1e-9)
 {
-	BetheAnsatzIntegrals::u = 0.25*U_input;
-	return seminfigrate(BetheAnsatzIntegrals::e0_integrand, 0., err,err);
+	Integrals::u = 0.25*U_input;
+	return seminfigrate(Integrals::e0_integrand, 0., err,err);
 }
 
-double BetheAnsatz_gap (double U_input, double err=1e-9)
+double gap (double U_input, double err=1e-9)
 {
-	BetheAnsatzIntegrals::u = 0.25*U_input;
-	return -2. + 2.*BetheAnsatzIntegrals::u + seminfigrate(BetheAnsatzIntegrals::gap_integrand, 0., err,err);
+	Integrals::u = 0.25*U_input;
+	return -2. + 2.*Integrals::u + seminfigrate(Integrals::gap_integrand, 0., err,err);
 }
+
+// Essler p. 218
+double spinon_v (double U_input)
+{
+	double x = 2.*M_PI/U_input;
+	return (U_input==0.)? 2.:2.*gsl_sf_bessel_I1(x)/gsl_sf_bessel_I0(x);
+}
+
+};
 
 #endif
