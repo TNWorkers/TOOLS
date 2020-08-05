8a4607d42296617bbdba7d59731ac70199f5d478
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2020-08-05 14:09:17 +0200

    TerminalPlot.h bietet ein Interface zur externen Terminal Plot-Bibliothek: https://github.com/fbbdev/plot Die Hauptfunktion ist TerminalPlot::plot(Container x,Container y,params), es gibt auch TerminalPlot::plot(Container y,params) und TerminalPlot::plot(Eigen::Array y,params). Ich musste bei der plot Bibliothek allerdings noch ticks und labels hinzufuegen, von daher geht es nur mit meiner Version der plot Bibliothek. Die bisher noch nicht gepushed, ich werde dafuer wahrscheinlich bei github einen fork machen. Offene Punkte/Probleme: 1) Bisher werden die Punkte immer verbunden, eine Linestyle-Option mit Werten "an" und "aus" gibt es bisher nicht. 	   					2) Der Aufruf ist in c++-17 ein bisschen bloed, weil die Parameter (gesammelt struct PlotParams) nicht so einfach uebergeben werden koennen. 						   In c++-20 gibt es designated intializers, damit waere es besser. Aber der Code kompiliert momentan nicht in c++-20. 						   Vielleicht nutzt man besser den ParamHandler... 						3) Histogramm waere noch gut, also TerminalPlot::bar 						4) Logarithmische Y-Achse waere auch gut, aber muss natuerlich nicht sein...

---
+++ TerminalPlot.h
@@ -0,0 +1,126 @@
+#ifndef TERMINAL_PLOT_H_
+#define TERMINAL_PLOT_H_
+
+#if __has_include("plot.hpp")
+#include "plot.hpp"
+#else
+#define DONT_USE_TERMINALPLOT
+#endif
+
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <numeric>
+
+#include "Ticker.h"
+
+struct Dummy {};
+struct PlotParams
+{
+	#ifdef DONT_USE_TERMINALPLOT
+	Dummy color;
+	#else
+	plot::Color color = plot::palette::steelblue;
+	#endif
+	std::string label = "";
+	std::size_t cellCols = 70;
+	std::size_t cellRows = 21;
+};
+
+namespace TerminalPlot
+{		
+	template<typename Container>
+	void plot(const Container &x, const Container &y, const PlotParams &params)
+	{
+		#ifdef DONT_USE_TERMINALPLOT
+		std::cout << "X: "; for (const auto &i:x) {std::cout << i << " ";} std::cout << std::endl;
+		std::cout << "Y: "; for (const auto &i:y) {std::cout << i << " ";} std::cout << std::endl;
+		#else
+		plot::TerminalInfo term;
+		term.detect();
+		plot::Coord canvasCellCols = params.cellCols;
+		plot::Coord canvasCellRows = params.cellRows;
+		plot::Size canvasCellSize(canvasCellCols, canvasCellRows);
+
+		const auto [it_xmin,it_xmax] = std::minmax_element(std::begin(x), std::end(x));
+		const auto [it_ymin,it_ymax] = std::minmax_element(std::begin(y), std::end(y));
+		typename Container::value_type xmin = *it_xmin;
+		typename Container::value_type xmax = *it_xmax;
+		typename Container::value_type ymin = *it_ymin;
+		typename Container::value_type ymax = *it_ymax;
+		Ticker xticker(xmin,xmax,"X");
+		Ticker yticker(ymin,ymax,"Y");
+		typename Container::value_type xmin_rnd = xticker.min();
+		typename Container::value_type xmax_rnd = xticker.max();
+		typename Container::value_type ymin_rnd = yticker.min();
+		typename Container::value_type ymax_rnd = yticker.max();
+		plot::RealCanvas<plot::BrailleCanvas, typename Container::value_type> canvas({ {xmin_rnd,ymin_rnd}, {xmax_rnd,ymax_rnd} }, canvasCellSize, term);
+
+		//compute ticks and labels
+		std::vector<std::size_t> xticks_int(9);
+		std::size_t xstep = std::floor(params.cellCols/8);
+		std::generate(xticks_int.begin(), xticks_int.end(), [xstep, n=-xstep] () mutable {return n+=xstep;});
+		std::vector<std::size_t> yticks_int(5);
+		std::size_t ystep = std::floor(params.cellRows/4);
+		std::generate(yticks_int.begin(), yticks_int.end(), [ystep, n=-ystep] () mutable {return n+=ystep;});
+		std::cout << "xticks: "; for (const auto tick:xticks_int) {std::cout << tick << " ";} std::cout << std::endl;
+		std::cout << "yticks: "; for (const auto tick:yticks_int) {std::cout << tick << " ";} std::cout << std::endl;
+		auto unmap = [&canvas, xmin_rnd](const std::vector<std::size_t> &ticks_int, bool X) {
+					   Container out(ticks_int.size());
+					   for (std::size_t i=0; i<ticks_int.size(); i++)
+					   {
+						   if (X) { out[i] = canvas.unmap(plot::Point(2*ticks_int[i],0)).x; }
+						   else   { out[i] = canvas.unmap(plot::Point(0,4*ticks_int[i])).y; }
+					   }
+					   return out;
+				   };
+		
+		Container xticks = unmap(xticks_int,true);
+		Container yticks = unmap(yticks_int,false);
+		std::vector<std::pair<std::size_t,std::string> > xtickLabels = xticker.labels(xticks, xticks_int);
+		std::vector<std::pair<std::size_t,std::string> > ytickLabels = yticker.labels(yticks, yticks_int);
+				
+		std::vector<plot::GenericPoint<typename Container::value_type> > points;
+		for (std::size_t i=0; i<x.size(); i++)
+		{
+			points.push_back(plot::GenericPoint<typename Container::value_type>(x[i],y[i]));
+		}
+		
+		canvas.path(params.color, points.begin(), points.end());		
+		std::cout << plot::margin(1, 2, 0, 2, plot::frame(xticks_int, yticks_int, xtickLabels, ytickLabels, params.label, plot::Align::Center, {plot::BorderStyle::Double, true}, &canvas, term)) << std::flush;
+		#endif
+	}
+
+	template<typename Container>
+	void plot(const Container &y, const PlotParams &params)
+	{
+		Container x(y.size());
+		for (size_t i=0; i<x.size(); i++)
+		{
+			x[i] = typename Container::value_type(i);
+		}
+		plot(x,y,params);
+	}
+
+	template<typename Scalar>
+	void plot(const Eigen::Array<Scalar,-1,1> &y, const PlotParams &params)
+	{
+		std::vector<Scalar> yvec(y.size());
+		for (size_t i=0; i<y.size(); i++)
+		{
+			yvec[i] = y(i);
+		}
+		std::vector<Scalar> x(y.size());
+		for (size_t i=0; i<x.size(); i++)
+		{
+			x[i] = Scalar(i);
+		}
+		#ifdef DONT_USE_TERMINALPLOT
+		std::cout << y.transpose() << std::endl;
+		#else
+		plot(x,yvec,params);
+		#endif
+	}
+}
+#endif
+
---
+++ Ticker.h
@@ -0,0 +1,137 @@
+#ifndef TICKER_H_
+#define TICKER_H_
+namespace TerminalPlot
+{
+	enum ROUND
+	{
+		TO_UPPER,
+		TO_LOWER,
+		AUTO
+	};
+
+	template<typename Scalar>
+	class Ticker
+	{
+	public:
+		Ticker () {}
+		Ticker (const Scalar min, const Scalar max, std::string label="")
+			: min_(min), max_(max)
+			{
+				min_rnd = round(2, min_, ROUND::TO_LOWER);
+				max_rnd = round(2, max_, ROUND::TO_UPPER);
+				std::cout << label << "-ticker: min~" << min_rnd << ", max~" << max_rnd << ", relevant Digits: " << relevantDigits() << endl;
+			};
+		std::vector<Scalar> ticks(std::size_t amountOfTicks = 7) const;
+		Scalar min() const {return min_rnd;}
+		Scalar max() const {return max_rnd;}
+		int relevantDigits() const;
+		Scalar round(int significant_digits, const Scalar in, ROUND mode) const;
+		std::vector<std::pair<std::size_t, std::string> > labels(std::vector<Scalar> ticks, std::vector<std::size_t> ticks_int) const
+			{
+				std::vector<std::pair<std::size_t, std::string> > out(ticks.size());
+				for (std::size_t i=0; i<ticks.size(); i++)
+				{
+					std::stringstream ss;
+					ss << std::fixed << std::setprecision(std::abs(relevantDigits())) << ticks[i];
+					out[i] = std::make_pair(ticks_int[i],ss.str());
+				}
+				return out;
+			}
+	private:
+		Scalar roundToLeadingDigit(const Scalar in, ROUND mode) const;
+		Scalar roundToDigit(int digit, const Scalar in, ROUND mode) const;
+		const Scalar min_, max_;
+		Scalar min_rnd, max_rnd;
+	};
+
+	template<typename Scalar>
+	std::vector<Scalar> Ticker<Scalar>::
+	ticks(std::size_t amountOfTicks) const
+	{
+		std::vector<Scalar> ticks_(amountOfTicks+1);
+		auto delta = (max_rnd-min_rnd)/amountOfTicks;
+		delta = roundToLeadingDigit(delta, ROUND::TO_LOWER) ;
+		ticks_[0] = min_rnd;
+		std::size_t i=1;
+		auto tick = ticks_[0]+delta;
+		while ( i < ticks_.size() and tick <= max_rnd)
+		{
+			ticks_[i] = tick;
+			i++;
+			tick += delta;
+		}
+		return ticks_;
+	}
+
+	template<typename Scalar>
+	int Ticker<Scalar>::
+	relevantDigits() const
+	{
+		auto tmp = max_rnd - min_rnd;
+		for (int power=-10; power<=10; power++)
+		{
+			Scalar transformed = std::pow(10,power) * tmp;
+			// std::cout << "pow=" << power << ", trans=" << transformed << std::endl;
+			if (transformed >= 1. and transformed <= 10.)
+			{
+				return power+1;
+			}
+		}
+		assert(false and "Could not round sensfully...");
+	}
+	
+	template<typename Scalar>
+	Scalar Ticker<Scalar>::
+	roundToLeadingDigit(const Scalar in, ROUND mode) const
+	{
+		if (in == Scalar(0)) {return in;}
+		auto tmp=in;
+		for (int power=-10; power<=10; power++)
+		{
+			Scalar transformed = std::pow(10,power) * tmp;
+			// std::cout << "pow=" << power << ", trans=" << transformed << std::endl;
+			if (transformed >= 1. and transformed <= 10.)
+			{
+				if (mode == ROUND::AUTO)
+					return std::round(transformed) * std::pow(10,-power);
+				else if (mode == ROUND::TO_UPPER)
+					return std::ceil(transformed) * std::pow(10,-power);
+				else if (mode == ROUND::TO_LOWER)
+					return std::floor(transformed) * std::pow(10,-power);
+			}
+		}
+		assert(false and "Could not round sensfully...");
+	}
+
+	template<typename Scalar>
+	Scalar Ticker<Scalar>::
+	round(int significant_digits, const Scalar in, ROUND mode) const
+	{
+		if (in == 0.) {return in;}
+
+		Scalar factor = std::pow(10.0, significant_digits - std::ceil(std::log10(std::abs(in))));
+		switch (mode)
+		{
+		case ROUND::AUTO: return std::round(in * factor) / factor;
+		case ROUND::TO_UPPER: return std::ceil(in * factor) / factor;
+		case ROUND::TO_LOWER: return std::floor(in * factor) / factor;
+		}
+		assert(false and "strange...");
+	}
+	
+	template<typename Scalar>
+	Scalar Ticker<Scalar>::
+	roundToDigit(int digit, const Scalar in, ROUND mode) const
+	{
+		Scalar transformed = std::pow(10,digit) * in;
+		switch (mode)
+		{
+		case ROUND::AUTO: return std::round(transformed) * std::pow(10,-digit);
+		case ROUND::TO_UPPER: return std::ceil(transformed) * std::pow(10,-digit);
+		case ROUND::TO_LOWER: return std::floor(transformed) * std::pow(10,-digit);
+		}
+	}
+
+}
+
+#endif
